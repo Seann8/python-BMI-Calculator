5638f98fcae326082a2a51b659c66f29eb4f85b6
Sean Nolan <seannolan@fb.com>
2018-06-04 18:39:40 +0100

    calc calss v2 , test cases added

--- calc.py
+++ calc.py
@@ -1,34 +1,46 @@
 ## main class for calculating bmi
 
 
-class Bmi(self):
+class Bmi():
     """@DESC main class for handling the units and inputs, converting and calculating the output to be returned """
+
     def __init__(self,weight,height,unit_weight,unit_height):
-        weight = self.weight
-        height = self.height
-        unit_height = self.unit_height
+        self.weight = weight
+        self.height = height
+        self.negerror = ValueError('Please enter positive values only')
+        self.valerror = TypeError('Please enter an integer or float only')
 
-    def convert_weight(self):
 
-        if self.unit_height == "pound":
-            convert = 0.45359237
-        elif self.unit_height == "stone":
-            convert = 0.15747
 
-        weight = self.weight/convert
 
-    return weight
-
-   def convert_height(self):
-       if self.unit_height == 'feet' :
-           convert = .3048
-           
     def calculate(self):
-        BMI =(weight / height ** 2)
-        return BMI
 
 
-    def convert(self):
-        pass
+        if isinstance(self.weight,int):
+            pass
+        elif isinstance(self.height,int):
+            pass
+
+        else:
+            raise self.valerror
+
+
+        if self.weight < 0 :
+            raise self.negerror
+        elif  self.height < 0 :
+            raise self.negerror
+
+        try:
+
+            bmi =(self.weight / self.height ** 2)
+
+        except ZeroDivisionError:
+
+            bmi = 0
+
+        return round(bmi,2)
+
+
+  
 
 
--- test_calc.py
+++ test_calc.py
@@ -1,15 +1,36 @@
-import calc
+from calc import Bmi
 import unittest
 
-class Test_Bmi_Calc (unittest.Testcase):
-    def test_height_convert(self):
-        pass
+class Test_Bmi_Calc (unittest.TestCase):
+
+
+    def setUp(self):
+        self.test_obj_calc = Bmi(130,1.92,'kg','m')
+
+        self.test_obj_zero = Bmi(0,0,'kg','m')
+
+        self.test_obj_neg = Bmi(-1,0,'kg','m')
+
+        self.test_obj_type = Bmi('1',0,'kg','m')
+
+    def tearDown(self):
+        del self.test_obj_calc
+        del self.test_obj_zero
+        del self.test_obj_neg
+        del self.test_obj_type
+
+  
 
     def test_bmi_calc(self):
-        self.assertEqual(calc.calculate(1.93,130),34.9)
 
-    def test_inputs(self):
-        pass
+        self.assertEqual(self.test_obj_calc.calculate(),35.26)
+
+        self.assertEqual(self.test_obj_zero.calculate(),0)
+
+        with self.assertRaises(ValueError) as context:
+            self.assertRaises(self.test_obj_neg.calculate())
+
+
 
 
 if __name__ == '__main__':
